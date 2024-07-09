a342203a6ac456c69a93f26dd95b95fb82cb3ea9
sushi <64286083+sushiroles@users.noreply.github.com>
2024-07-09 19:38:46 +0200

    New functions for reverse search

--- nebula_api/apple_music_nebula.py
+++ nebula_api/apple_music_nebula.py
@@ -8,6 +8,29 @@ except:
 
 
 
+def is_apple_music_track(url: str):
+	if url.find('https://music.apple.com/') >= 0 and url.find('?i=') >= 0:
+		return True
+	else:
+		return False
+
+def is_apple_music_album(url: str):
+	if url.find('https://music.apple.com/') >= 0 and not url.find('?i=') >= 0:
+		return True
+	else:
+		return False
+	
+def get_apple_music_track_id(url: str):
+	index = url.index('?i=') + 3
+	return url[index:]
+
+def get_apple_music_album_id(url: str):
+	index = len(url) - 1
+	while url[index] != '/':
+		index -= 1
+	return url[index+1:]
+
+	
 def search_apple_music_track(artist, track):
 	tracks_data = []
 
@@ -61,5 +84,61 @@ def search_apple_music_album(artist, album):
 				'cover': cover,
 			})
 		return filter_album(artist, album, albums_data)
+	else:
+		return None
+
+
+
+def get_apple_music_track(identifier: str):
+	track_data = []
+
+	url = f"https://itunes.apple.com/lookup?id={identifier}"
+	response = requests.get(url)
+	result = response.json()['results'][0]
+
+	if result != []:
+		url = str(result['trackViewUrl'])
+		identifier = str(result['trackId'])
+		artists = split_artists(str(result['artistName']))
+		title = str(result['trackName'])
+		year = str(result['releaseDate'][:4])
+		cover = str(result['artworkUrl100'])
+		track_data.append({
+			'url': url,
+			'id': identifier,
+			'artists': artists,
+			'track': title,
+			'year': year,
+			'cover': cover,
+		})
+		return track_data[0]
+	else:
+		return None
+
+
+
+def get_apple_music_album(identifier: str):
+	object_data = []
+
+	url = f"https://itunes.apple.com/lookup?id={identifier}"
+	response = requests.get(url)
+	result = response.json()['results'][0]
+
+	if result != []:
+		url = str(result['collectionViewUrl'])
+		identifier = str(result['collectionId'])
+		artists = split_artists(str(result['artistName']))
+		title = str(result['collectionName'])
+		year = str(result['releaseDate'][:4])
+		cover = str(result['artworkUrl100'])
+		object_data.append({
+			'url': url,
+			'id': identifier,
+			'artists': artists,
+			'album': title,
+			'year': year,
+			'cover': cover,
+		})
+		return object_data[0]
 	else:
 		return None
