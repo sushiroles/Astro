ced5f4fa2f7fa685596de06251efbff28a7e1690
sushi <64286083+sushiroles@users.noreply.github.com>
2024-07-09 03:33:23 +0200

    Tidying up and extra functions
    get_common_data used as experiment for extra filtering, haven't gotten it to work properly *yet*

--- nebula_api/etc.py
+++ nebula_api/etc.py
@@ -18,20 +18,20 @@ def save_json(json_data):
 	with open('save_json_data.json', 'w', encoding = 'utf-8') as f:
 		json.dump(json_data, f, ensure_ascii = False, indent = 4)
 
-def remove_punctuation(string):
+def remove_punctuation(string: str):
 	punc = f'''!()-[];:'",<>./?@#$%^&*_~'''
 	for element in string:
 		if element in punc:
 			string = string.replace(element, '')
 	return string
 
-def replace_with_ascii(string):
+def replace_with_ascii(string: str):
 	return unidecode(string)
 
-def bare_bones(string):
+def bare_bones(string: str):
 	return replace_with_ascii(remove_punctuation(string)).lower()
 
-def split_artists(string):
+def split_artists(string: str):
 	strings = re.split(r",|\&", string)
 	fixed_strings = []
 	for element in strings:
@@ -41,3 +41,14 @@ def split_artists(string):
 			element = element[:-1]
 		fixed_strings.append(element)
 	return fixed_strings
+
+def get_common_data(data: list):
+	element_counts = {}
+	for element in data:
+		if element in element_counts and element != '':
+			element_counts[element] += 1
+		else:
+			element_counts[element] = 1
+	max_count = max(element_counts.values())
+	most_common = {key: value for key, value in element_counts.items() if value == max_count}
+	return list(most_common.items())
