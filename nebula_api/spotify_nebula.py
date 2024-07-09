40ebba55592ab01cda203af164c34f92a3798af6
sushi <64286083+sushiroles@users.noreply.github.com>
2024-07-09 19:42:09 +0200

    Functions for reverse search

--- nebula_api/spotify_nebula.py
+++ nebula_api/spotify_nebula.py
@@ -14,13 +14,29 @@ config.read('tokens.ini')
 
 
 
+def is_spotify_track(url: str):
+	if url.find('https://open.spotify.com/track/') >= 0:
+		return True
+	else:
+		return False
+	
+def is_spotify_album(url: str):
+	if url.find('https://open.spotify.com/album/') >= 0:
+		return True
+	else:
+		return False
+
+def get_spotify_id(url: str):
+	return url[31:53]
+
+
 def search_spotify_track(artist: str, track: str):
 	tracks_data = []
 
 	spotify_id = config['spotify']['id']
 	spotify_secret = config['spotify']['secret']
 	client_credentials_manager = SpotifyClientCredentials(spotify_id, spotify_secret)
-	sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+	sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
 	search_query = f'artist:{artist} track:{track}'
 	search_results = sp.search(q = search_query, type = 'track')
@@ -55,10 +71,10 @@ def search_spotify_album(artist: str, album: str):
 	spotify_id = config['spotify']['id']
 	spotify_secret = config['spotify']['secret']
 	client_credentials_manager = SpotifyClientCredentials(spotify_id, spotify_secret)
-	sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+	sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
 	search_query = f'artist:{artist} album:{album}'
-	search_results = sp.search(q=search_query, type='album')
+	search_results = sp.search(q = search_query, type = 'album')
 
 	if search_results['albums']['items']:
 		for result in search_results['albums']['items']:
@@ -81,3 +97,61 @@ def search_spotify_album(artist: str, album: str):
 		return filter_album(artist, album, albums_data)
 	else:
 		return None
+
+
+
+def get_spotify_track(identifier: str):
+	track_data = []
+
+	spotify_id = config['spotify']['id']
+	spotify_secret = config['spotify']['secret']
+	client_credentials_manager = SpotifyClientCredentials(spotify_id, spotify_secret)
+	sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+	track = sp.track(identifier)
+	url = str(track['external_urls']['spotify'])
+	identifier = str(track['id'])
+	artists = []
+	for names in track['artists']:
+		artists.append(str(names['name']))
+	title = str(track['name'])
+	year = str(track['album']['release_date'][:4])
+	cover = str(track['album']['images'][0]['url'])
+	track_data.append({
+		'url': url,
+		'id': identifier,
+		'artists': artists,
+		'track': title,
+		'year': year,
+		'cover': cover,
+	})
+	return track_data[0]
+
+
+
+def get_spotify_album(identifier: str):
+	album_data = []
+
+	spotify_id = config['spotify']['id']
+	spotify_secret = config['spotify']['secret']
+	client_credentials_manager = SpotifyClientCredentials(spotify_id, spotify_secret)
+	sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+	album = sp.album(identifier)
+	url = str(album['external_urls']['spotify'])
+	identifier = str(album['id'])
+	artists = []
+	for names in album['artists']:
+		artists.append(str(names['name']))
+	title = str(album['name'])
+	year = str(album['release_date'][:4])
+	cover = str(album['images'][0]['url'])
+	album_data.append({
+		'url': url,
+		'id': identifier,
+		'artists': artists,
+		'album': title,
+		'year': year,
+		'cover': cover,
+	})
+	return album_data[0]
