1cea9c123441647c8f4eee865c91024968be67a9
sushi <64286083+sushiroles@users.noreply.github.com>
2024-07-09 03:33:52 +0200

    Album honesty filter experiment
    doesn't work yet :(

--- nebula_api/filter.py
+++ nebula_api/filter.py
@@ -32,13 +32,58 @@ def filter_album(artist: str, album: str, albums_data: list):
 
 # WIP
 def album_honesty_filter(spotify_data, apple_music_data, youtube_music_data, deezer_data, tidal_data, bandcamp_data):
-	spotify = 0
-	apple_music = 1
-	youtube_music = 2
-	deezer = 3
-	tidal = 4
-	bandcamp = 5
+	services = {
+		'Spotify': 0,
+		'Apple Music': 1,
+		'Youtube Music': 2,
+		'Deezer': 3,
+		'TIDAL': 4,
+		'Bandcamp': 5,
+	}
+	
+	artist_names = [
+		bare_bones(' '.join(spotify_data['artists'])),
+		bare_bones(' '.join(apple_music_data['artists'])),
+		bare_bones(' '.join(youtube_music_data['artists'])),
+		bare_bones(' '.join(deezer_data['artists'])),
+		bare_bones(' '.join(tidal_data['artists'])),
+		bare_bones(' '.join(bandcamp_data['artists'])),
+	]
+	print(artist_names)
+	album_titles = [
+		spotify_data['album'],
+		apple_music_data['album'],
+		youtube_music_data['album'],
+		deezer_data['album'],
+		tidal_data['album'],
+		bandcamp_data['album'],
+	]
+	release_years = [
+		spotify_data['year'],
+		apple_music_data['year'],
+		youtube_music_data['year'],
+		deezer_data['year'],
+		tidal_data['year'],
+		bandcamp_data['year'],
+	]
+	anchors = [
+		spotify_data['anchor'],
+		apple_music_data['anchor'],
+		youtube_music_data['anchor'],
+		deezer_data['anchor'],
+		tidal_data['anchor'],
+		bandcamp_data['anchor'],
+	]
 
-	artist_names = [spotify_data['artist']]
-	album_names = []
-	release_years = []
+	common_artist_data = get_common_data(artist_names)
+	common_title_data = get_common_data(album_titles)
+	common_year_data = get_common_data(release_years)
+
+	final_anchors = ''
+	
+	for i in range(len(services)):
+		if bare_bones(common_title_data) == bare_bones(album_titles[i]):
+			if common_year_data == release_years[i] or release_years == '':
+				final_anchors += anchors[i]
+	return final_anchors
+
