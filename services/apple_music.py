f3d5211d32b030dba82b6e9de0fba1403c4c52f2
sushi <64286083+sushiroles@users.noreply.github.com>
2024-07-14 22:03:11 +0200

    Put everything in try cases
    iTunes API started acting up today and it would cause errors in the app

--- services/apple_music.py
+++ services/apple_music.py
@@ -30,107 +30,119 @@ def get_apple_music_album_id(url: str):
 
 	
 def search_apple_music_track(artist, track):
-	tracks_data = []
+	try:
+		tracks_data = []
 
-	url = f"https://itunes.apple.com/search?term={artist}+{track}&entity=song"
-	response = requests.get(url)
-	search_results = response.json()['results']
+		url = f"https://itunes.apple.com/search?term={artist}+{track}&entity=song"
+		response = requests.get(url)
+		search_results = response.json()['results']
 
-	if search_results != []:
-		for result in search_results:
+		if search_results != []:
+			for result in search_results:
+				url = str(result['trackViewUrl'])
+				identifier = str(result['trackId'])
+				artists = split_artists(str(result['artistName']))
+				title = str(result['trackName'])
+				year = str(result['releaseDate'][:4])
+				cover = str(result['artworkUrl100'])
+				tracks_data.append({
+					'url': url,
+					'id': identifier,
+					'artists': artists,
+					'track': title,
+					'year': year,
+					'cover': cover,
+				})
+			return filter_track(artist, track, tracks_data)
+		else:
+			return None
+	except:
+		return None
+
+
+
+def search_apple_music_album(artist, album):
+	try:
+		albums_data = []
+
+		url = f"https://itunes.apple.com/search?term={artist}+{album}&entity=album"
+		response = requests.get(url)
+		search_results = response.json()['results']
+
+		if search_results != []:
+			for result in search_results:
+				url = str(result['collectionViewUrl'])
+				identifier = str(result['collectionId'])
+				artists = split_artists(str(result['artistName']))
+				title = str(result['collectionName'])
+				year = str(result['releaseDate'][:4])
+				cover = str(result['artworkUrl100'])
+				albums_data.append({
+					'url': url,
+					'id': identifier,
+					'artists': artists,
+					'album': title,
+					'year': year,
+					'cover': cover,
+				})
+			return filter_album(artist, album, albums_data)
+		else:
+			return None
+	except:
+		return None
+
+
+
+def get_apple_music_track(identifier: str):
+	try:
+		url = f"https://itunes.apple.com/lookup?id={identifier}"
+		response = requests.get(url)
+		result = response.json()['results'][0]
+
+		if result != []:
 			url = str(result['trackViewUrl'])
 			identifier = str(result['trackId'])
 			artists = split_artists(str(result['artistName']))
 			title = str(result['trackName'])
 			year = str(result['releaseDate'][:4])
 			cover = str(result['artworkUrl100'])
-			tracks_data.append({
+			return {
 				'url': url,
 				'id': identifier,
 				'artists': artists,
 				'track': title,
 				'year': year,
 				'cover': cover,
-			})
-		return filter_track(artist, track, tracks_data)
-	else:
+			}
+		else:
+			return None
+	except:
 		return None
 
 
 
-def search_apple_music_album(artist, album):
-	albums_data = []
+def get_apple_music_album(identifier: str):
+	try:
+		url = f"https://itunes.apple.com/lookup?id={identifier}"
+		response = requests.get(url)
+		result = response.json()['results'][0]
 
-	url = f"https://itunes.apple.com/search?term={artist}+{album}&entity=album"
-	response = requests.get(url)
-	search_results = response.json()['results']
-
-	if search_results != []:
-		for result in search_results:
+		if result != []:
 			url = str(result['collectionViewUrl'])
 			identifier = str(result['collectionId'])
 			artists = split_artists(str(result['artistName']))
 			title = str(result['collectionName'])
 			year = str(result['releaseDate'][:4])
 			cover = str(result['artworkUrl100'])
-			albums_data.append({
+			return {
 				'url': url,
 				'id': identifier,
 				'artists': artists,
 				'album': title,
 				'year': year,
 				'cover': cover,
-			})
-		return filter_album(artist, album, albums_data)
-	else:
-		return None
-
-
-
-def get_apple_music_track(identifier: str):
-	url = f"https://itunes.apple.com/lookup?id={identifier}"
-	response = requests.get(url)
-	result = response.json()['results'][0]
-
-	if result != []:
-		url = str(result['trackViewUrl'])
-		identifier = str(result['trackId'])
-		artists = split_artists(str(result['artistName']))
-		title = str(result['trackName'])
-		year = str(result['releaseDate'][:4])
-		cover = str(result['artworkUrl100'])
-		return {
-			'url': url,
-			'id': identifier,
-			'artists': artists,
-			'track': title,
-			'year': year,
-			'cover': cover,
-		}
-	else:
-		return None
-
-
-
-def get_apple_music_album(identifier: str):
-	url = f"https://itunes.apple.com/lookup?id={identifier}"
-	response = requests.get(url)
-	result = response.json()['results'][0]
-
-	if result != []:
-		url = str(result['collectionViewUrl'])
-		identifier = str(result['collectionId'])
-		artists = split_artists(str(result['artistName']))
-		title = str(result['collectionName'])
-		year = str(result['releaseDate'][:4])
-		cover = str(result['artworkUrl100'])
-		return {
-			'url': url,
-			'id': identifier,
-			'artists': artists,
-			'album': title,
-			'year': year,
-			'cover': cover,
-		}
-	else:
+			}
+		else:
+			return None
+	except:
 		return None
