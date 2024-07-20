717ecae98ad314f15e0486dd5d5364294950d174
sushi <64286083+sushiroles@users.noreply.github.com>
2024-07-20 04:38:45 +0200

    Tweaks for accuracy and edge cases

--- services/filter.py
+++ services/filter.py
@@ -3,16 +3,18 @@ try:
 except:
 	from etc import *
 
+
+
 def filter_track(artist: str, track: str, tracks_data: list):
 	artist = bare_bones(artist)
-	track = bare_bones(track)
+	track = bare_bones(track, False)
 	for scans in range(2):
 		for data in tracks_data:
 			data_artists = []
 			for name in data['artists']:
-				data_artists.append(bare_bones(name))
+				data_artists.append(bare_bones(name, True))
 			artists_string = ' '.join(data_artists)
-			data_track = bare_bones(data['track'])
+			data_track = bare_bones(data['track'], False)		
 			if scans == 0:
 				condition = artists_string.find(artist) >= 0 and data_track == track
 			else:
@@ -21,16 +23,18 @@ def filter_track(artist: str, track: str, tracks_data: list):
 				return data
 	return None
 
+
+
 def filter_album(artist: str, album: str, albums_data: list):
 	artist = bare_bones(artist)
-	album = bare_bones(album)
+	album = bare_bones(album, False)
 	for scans in range(2):
 		for data in albums_data:
 			data_artists = []
 			for name in data['artists']:
 				data_artists.append(bare_bones(name))
 			artists_string = ' '.join(data_artists)
-			data_album = bare_bones(data['album'])
+			data_album = bare_bones(data['album'], False)
 			if scans == 0:
 				condition = artists_string.find(artist) >= 0 and data_album == album
 			else:
