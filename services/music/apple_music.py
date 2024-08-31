467f0e7f679663f6f21487b3dfaa5a6c4b137618
sushi <64286083+sushiroles@users.noreply.github.com>
2024-08-31 21:27:02 +0200

    optimizations for 1.1 + bug fix
    fixed a bug where music video searching didn't work

--- services/music/apple_music.py
+++ services/music/apple_music.py
@@ -54,6 +54,14 @@ def get_apple_music_album_video_id(url: str):
 			'country_code': url[24:26],
 		}
 
+def clean_up_collection_title(string: str):
+	if ' - Single' in string:
+		return string.replace(' - Single','')
+	elif ' - EP' in string:
+		return string.replace(' - EP','')
+	else:
+		return string
+
 async def get_apple_music_artist(artist_id: int):
 	async with aiohttp.ClientSession() as session:
 		api_url = f"https://itunes.apple.com/lookup?id={artist_id}"
@@ -85,7 +93,7 @@ async def get_apple_music_track(identifier: str, country_code: str):
 					track_title = result['trackName']
 					track_artists = await get_apple_music_artist(result['artistId'])
 					track_cover = result['artworkUrl100']
-					track_collection = result['collectionName']
+					track_collection = clean_up_collection_title(result['collectionName']) if 'song' in result['kind'] else None
 					track_is_explicit = not 'not' in result['trackExplicitness']
 					return {
 						'type': 'track',
@@ -128,6 +136,7 @@ async def get_apple_music_album(identifier: str, country_code: str):
 					album_cover = result['artworkUrl100']
 					album_year = result['releaseDate'][:4]
 					if ' - Single' in album_title:
+						track_is_explicit = not 'not' in result['collectionExplicitness']
 						return {
 							'type': 'track',
 							'url': album_url,
@@ -135,6 +144,8 @@ async def get_apple_music_album(identifier: str, country_code: str):
 							'title': album_title.replace(' - Single',''),
 							'artists': album_artists,
 							'cover': album_cover,
+							'collection_name': album_title.replace(' - Single',''),
+							'is_explicit': track_is_explicit,
 							'extra': {
 								'api_time_ms': current_time_ms() - start_time,
 								'response_status': f'AppleMusic-{response.status}'
@@ -153,9 +164,9 @@ async def get_apple_music_album(identifier: str, country_code: str):
 						'cover': album_cover,
 						'year': album_year,
 						'extra': {
-								'api_time_ms': current_time_ms() - start_time,
-								'response_status': f'AppleMusic-{response.status}'
-							}
+							'api_time_ms': current_time_ms() - start_time,
+							'response_status': f'AppleMusic-{response.status}'
+						}
 					}
 				elif response.status == 503:
 					await asyncio.sleep(0.1)
@@ -171,7 +182,9 @@ async def get_apple_music_album(identifier: str, country_code: str):
 async def search_apple_music_track(artist: str, track: str, collection: str = None, is_explicit: bool = None):
 	tracks_data = []
 	async with aiohttp.ClientSession() as session:
-		query = f'{artist}+{track}'
+		query = f'{artist}+"{track}"'
+		if collection != None:
+			query = f'{artist}+"{track}"+{collection}'
 		api_url =f'https://itunes.apple.com/search?term={query}&entity=song&limit=200&country=us'
 		start_time = current_time_ms()
 		while True:
@@ -186,7 +199,7 @@ async def search_apple_music_track(artist: str, track: str, collection: str = No
 							track_title = item['trackName']
 							track_artists = split_artists(item['artistName'])
 							track_cover = item['artworkUrl100']
-							track_collection = item['collectionName']
+							track_collection = clean_up_collection_title(item['collectionName'])
 							track_is_explicit = not 'not' in item['trackExplicitness']
 							tracks_data.append({
 								'type': 'track',
@@ -199,7 +212,7 @@ async def search_apple_music_track(artist: str, track: str, collection: str = No
 								'is_explicit': track_is_explicit,
 								'extra': {
 									'api_time_ms': current_time_ms() - start_time,
-									'response_status': str(response.status)
+									'response_status': f'AppleMusic-{response.status}'
 								}
 							})
 						return filter_track(tracks_data = tracks_data, artist = artist, track = track, collection = collection, is_explicit = is_explicit)
@@ -221,7 +234,7 @@ async def search_apple_music_track(artist: str, track: str, collection: str = No
 async def search_apple_music_album(artist: str, album: str, year: str = None):
 	albums_data = []
 	async with aiohttp.ClientSession() as session:
-		query = f'{artist}+{album}'
+		query = f'{artist}+"{album}"'
 		api_url =f'https://itunes.apple.com/search?term={query}&entity=album&limit=200&country=us'
 		start_time = current_time_ms()
 		while True:
@@ -246,9 +259,9 @@ async def search_apple_music_album(artist: str, album: str, year: str = None):
 								'cover': album_cover,
 								'year': album_year,
 								'extra': {
-										'api_time_ms': current_time_ms() - start_time,
-										'response_status': f'AppleMusic-{response.status}'
-									}
+									'api_time_ms': current_time_ms() - start_time,
+									'response_status': f'AppleMusic-{response.status}'
+								}
 							})
 						return filter_album(albums_data = albums_data, artist = artist, album = album, year = year)
 					else:
