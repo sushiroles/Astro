0c435a5f8067fbabe37b601e953195865b89ef69
sushi <64286083+sushiroles@users.noreply.github.com>
2024-08-31 21:26:20 +0200

    more advanced filtering functions
    did this so i can differentiate album and single versions of songs, albums with same titles from the same artists (looking at you weezer) and to differentiate explicit versions of songs. might still need tweaking

--- services/filter.py
+++ services/filter.py
@@ -6,51 +6,81 @@ except:
 
 
 def filter_track(tracks_data: list, artist: str, track: str, collection: str = None, is_explicit: bool = None):
-	artist = bare_bones(artist)
-	track = optimize_string(track)
-	data_with_percentage = []
+	max_score = 2000
+	if collection != None:
+		max_score += 1000
+	if is_explicit != None:
+		max_score += 500
+
+	data_with_similarity = []
 	for data in tracks_data:
-		data_artists = []
-		for name in data['artists']:
-			data_artists.append(bare_bones(name))
-		artists_string = ' '.join(data_artists)
-		data_track = bare_bones(data['title'], False)
-		num_of_found_words = 0
-		if artists_string.find(artist) >= 0:
-			for word in track:
-				if data_track.find(word) >= 0:
-					num_of_found_words += 1
-			similarity_percentage = 100 / len(data_track.split()) * num_of_found_words
-			data_with_percentage.append([similarity_percentage, data])
-	data_with_percentage = sorted(data_with_percentage, key = lambda x: x[0], reverse = True)
-	if data_with_percentage != []:
-		return data_with_percentage[0][1]
+		track_similarity = 0
+
+		artist_input = bare_bones(artist)
+		artists_reference = data['artists']
+		artists_with_similarity = []
+		for artist_name in artists_reference:
+			artists_with_similarity.append([calculate_similarity(bare_bones(artist_name), artist_input), artist_name])
+		artists_with_similarity = sort_similarity_lists(artists_with_similarity)
+		if artists_with_similarity != []:
+			track_similarity += artists_with_similarity[0][0]
+
+		title_input = bare_bones(track)
+		title_reference = remove_feat(data['title'])
+		track_similarity += calculate_similarity(bare_bones(title_reference), title_input)
+
+		if collection != None:
+			collection_input = bare_bones(collection)
+			collection_reference = data['collection_name']
+			track_similarity += calculate_similarity(bare_bones(collection_reference), collection_input)
+
+		if is_explicit != None:
+			if is_explicit == data['is_explicit']:
+				track_similarity += 500
+
+		data_with_similarity.append([track_similarity, data])
+	
+	data_with_similarity = sort_similarity_lists(data_with_similarity)
+	if percentage(max_score, data_with_similarity[0][0]) > 30:
+		return data_with_similarity[0][1]
 	else:
 		return {
 			'type': 'empty_response'
 		}
 
-	
+
+
 def filter_album(albums_data: list, artist: str, album: str, year: str = None):
-	artist = bare_bones(artist)
-	album = optimize_string(album)
-	data_with_percentage = []
+	max_score = 2000
+	if year != None:
+		max_score += 1000
+
+	data_with_similarity = []
 	for data in albums_data:
-		data_artists = []
-		for name in data['artists']:
-			data_artists.append(bare_bones(name))
-		artists_string = ' '.join(data_artists)
-		data_album = bare_bones(data['title'], False)
-		num_of_found_words = 0
-		if artists_string.find(artist) >= 0:
-			for word in album:
-				if data_album.find(word) >= 0:
-					num_of_found_words += 1
-			similarity_percentage = 100 / len(data_album.split()) * num_of_found_words
-			data_with_percentage.append([similarity_percentage, data])
-	data_with_percentage = sorted(data_with_percentage, key = lambda x: x[0], reverse = True)
-	if data_with_percentage != []:
-		return data_with_percentage[0][1]
+		track_similarity = 0
+
+		artist_input = bare_bones(artist)
+		artists_reference = data['artists']
+		artists_with_similarity = []
+		for artist_name in artists_reference:
+			artists_with_similarity.append([calculate_similarity(bare_bones(artist_name), artist_input), artist_name])
+		artists_with_similarity = sort_similarity_lists(artists_with_similarity)
+		if artists_with_similarity != []:
+			track_similarity += artists_with_similarity[0][0]
+
+		title_input = bare_bones(album)
+		title_reference = remove_feat(data['title'])
+		track_similarity += calculate_similarity(bare_bones(title_reference), title_input)
+
+		if year != None:
+			if year == data['year']:
+				track_similarity += 1000
+
+		data_with_similarity.append([track_similarity, data])
+	
+	data_with_similarity = sort_similarity_lists(data_with_similarity)
+	if percentage(max_score, data_with_similarity[0][0]) > 30:
+		return data_with_similarity[0][1]
 	else:
 		return {
 			'type': 'empty_response'
